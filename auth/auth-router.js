74f9869dce704424b77188d9285c90ba39e478be
adastraz <tylergosox@gmail.com>
2020-03-02 11:33:01 -0500

    heroku start command

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -28,10 +28,7 @@ router.post('/login', (req, res) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user)
 
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-          token
-        });
+        res.status(200).json({ user, token });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -43,8 +40,9 @@ router.post('/login', (req, res) => {
 
 function generateToken (user) {
   const payload = {
-    subject: user.id,
-    username: user.username
+    id: user.id,
+    username: user.username,
+    user_type: user.user_type
   }
   const options = {
     expiresIn: '1h',
